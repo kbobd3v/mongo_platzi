e5a88c382bb4e696a19994fb962e2ee87193d9bb
Kbob <leonard@airp.ws>
2021-09-24 12:45:22 -0500

    final version of platzi api

--- platzi-api/db.py
+++ platzi-api/db.py
@@ -24,60 +24,72 @@ def collection_stats(collection_nombre):
 
 # -----------------Carreras-------------------------
 
-
+# La función crear carrera recibe como parametro el json que llega desde la api rest
+# luego inserta la información del json en la coleccion carreras y devuelve el id de la carrera insertada
 def crear_carrera(json):
-    return str('Falta por implementar')
+    return str(db.carreras.insert_one(json).inserted_id)
 
-
+# la función consultar carrera por id recibe como parametro el id de la carrera que queremos buscar en la base de datos
+# usamos el metodo dumps que convierte el bson a json
+# usamos el comando find one donde agregamos el _id como string y en el ObjectId pasamos el id de la carrera que viene del json
 def consultar_carrera_por_id(carrera_id):
-    return str('Falta por implementar')
+    return dumps(db.carreras.find_one({'_id': ObjectId(carrera_id)}))
 
-
+# Actualizar carrera recibe un json de carrera,en postman debe modificarse el id que viene en el json por el id de la carrera que creamos anteriormente
+# para que el filtro lo encuentre y ejecute los cambios
 def actualizar_carrera(carrera):
     # Esta funcion solamente actualiza nombre y descripcion de la carrera
-    return str('Falta por implementar')
+    return str(db.carreras.update_one({'_id': ObjectId(carrera['_id'])}, {'$set': {'nombre': carrera['nombre'], 'descripcion': carrera['descripcion']}}).modified_count)
 
-
+# Borrar carrera toma especificamente carrera_id y lo convierte en el ObjectId para usarlo como filtro y asi poder eliminar el documento
 def borrar_carrera_por_id(carrera_id):
-    return str('Falta por implementar')
+    return str(db.carreras.delete_one({'_id': ObjectId(carrera_id)}))
 
 
 # Clase de operadores
 def consultar_carreras(skip, limit):
     return dumps(db.carreras.find({}).skip(int(skip)).limit(int(limit)))
 
-
+# Agregar curso a carrera toma nuestro json como parametro
+# Declara una variable curso que filtra el curso que queremos agregar, por defecto trae el id del curso y en la proyeccion pedimos que traiga el nombre
+# luego ejecuta la actualización de la carrera filtrando la carrera que queremos editar
+# y con  $addToSet agregamos la información del curso al documento de la carrera en el arreglo de cursos, por eso usamos el operador $addToSet
 def agregar_curso(json):
-    return str('Falta por implementar')
+    curso = consultar_curso_por_id_proyeccion(json['id_curso'], proyeccion={'nombre':1})
+    return str(db.carreras.update_one({'_id': ObjectId(json['id_carrera'])}, {'$addToSet': {'cursos': curso}}).modified_count)
 
 
 def borrar_curso_de_carrera(json):
-    return str('Falta por implementar')
+    return str(db.carreras.update_one({'_id': ObjectId(json['id_carrera'])}, {'$pull':{'cursos': {'_id': ObjectId(json['id_curso'])}}}).modified_count)
 
 # -----------------Cursos-------------------------
 
-
+# Crear curso recibe un json de parametro que contiene el documento que queremos agregar a la colección de cursos
+# retorna el id del curso creado
 def crear_curso(json):
-    return str('Falta por implementar')
+    return str(db.cursos.insert_one(json).inserted_id)
 
-
+# Consultar curso recibe el id_curso como parametro y ejecuta la busqueda agregando id_curso dentro del objectid del filtro
 def consultar_curso_por_id(id_curso):
-    return str('Falta por implementar')
+    return dumps(db.cursos.find_one({'_id': ObjectId(id_curso)}))
 
-
+# Actualizar curso toma el json curso y realiza una busqueda en la base de datos de un curso determinado por el id y luego con $set modifica la información del curso
+# Y retorna la cantidad de documentos modificados
 def actualizar_curso(curso):
     # Esta funcion solamente actualiza nombre, descripcion y clases del curso
-    return str('Falta por implementar')
-
+    filter = {"_id": ObjectId(curso["_id"])}
+    set = {'$set': {'nombre': curso['nombre'], 'descripcion': curso['descripcion'], 'clases': curso['clases']}}
+    return str(db.cursos.update_one(filter, set).modified_count)
 
 def borrar_curso_por_id(curso_id):
-    return str('Falta por implementar')
+    return str(db.cursos.delete_one({'_id': ObjectId(curso_id)}).delete_count)
 
 
+# Consultar curso por ID proyección para obtener la información del curso que vamos a guardar en nuestra coleccion de carreras
 def consultar_curso_por_id_proyeccion(id_curso, proyeccion=None):
-    return str('Falta por implementar')
+    return db.cursos.find_one({'_id': ObjectId(id_curso)}, proyeccion)
 
 
 def consultar_curso_por_nombre(nombre):
-    return str('Falta por implementar')
+    return dumps(db.cursos.find({'$text': {'$search': nombre}}))
 
